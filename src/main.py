d48cf243a20a4d6d337a617624ee2299789cb813
Aditiya <aditiyaart23@gmail.com>
2025-04-11 17:46:26 +1200

    Modified grab method

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-import keyboard as kb
-from automaton import Automaton
+from automaton.automaton import Automaton
+from limbus.dungeon import Dungeon
+from limbus.data import Config
 from time import sleep
+import cv2
+import keyboard as kb
 
 def Toggle(automaton: Automaton):
     if automaton.state:
@@ -22,6 +25,18 @@ def grabber(automaton: Automaton):
     automaton.zoom(-6)
     automaton.drag(400, 300)
     automaton.grab()
+
+    encounters = "D:\Repository\python-limbus\images\encounter"
+    img = cv2.imread("D:\Repository\python-limbus\images\capture.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    md = Dungeon(img, config=Config(), encounters_dir=encounters)
+
+    md.map()
+    md.find_lines()
+    md.map_connections()
+    path = md.crawl()
+
+    print(f"Shortest/Most rewarding path: {path}")
     
 def log(automaton: Automaton):
     print(automaton.dir)
@@ -31,7 +46,7 @@ def region(automaton: Automaton):
 
 def main():
     atom = Automaton("LimbusCompany")
-    kb.add_hotkey("ctrl+r", Toggle, [atom])
+    kb.add_hotkey("ctrl+e", Toggle, [atom])
     kb.add_hotkey("ctrl+d", region, [atom])
     kb.add_hotkey("ctrl+t", grabber, [atom])
     kb.add_hotkey("ctrl+g", log, [atom])
