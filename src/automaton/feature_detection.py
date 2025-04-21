e027c2314041ba8b2e1d394efcd7fff07b8db117
Aditiya <aditiyaart23@gmail.com>
2025-04-21 16:04:53 +1200

    Added brute force KNN matcher

--- src/automaton/feature_detection.py
+++ src/automaton/feature_detection.py
@@ -7,11 +7,22 @@ def detect_feature(img: MatLike, edge_threshold=31):
 
     return keypoints, descriptor
 
-def match_feature(descriptor_query, descriptor_train, sort=False):
-    matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+def match_feature(descriptor_query, descriptor_train, cross_check: bool=True, sort: bool=False):
+    matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=cross_check)
     matches = matcher.match(descriptor_query, descriptor_train)
 
     if sort == True:
         matches = sorted(matches, key=lambda x:x.distance)
     
     return matches
+
+def detect_object(target_descriptor, train_descriptor, ratio: float=0.75, min: int=10) -> bool:
+    matcher = cv.BFMatcher(cv.NORM_HAMMING)
+    matches = matcher.knnMatch(target_descriptor, train_descriptor, k=2)
+
+    candidates = []
+    for m, n in matches:
+        if m.distance < ratio*n.distance:
+            candidates.append([m])
+
+    return len(candidates) > min
